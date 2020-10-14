2f39f60f5328ec038bfda16f9517de873f133012
szalpal <mszolucha@nvidia.com>
2020-10-14 02:37:31 +0200

    Add automatic tests for dali_backend: identity and rn50_ensemble
    Signed-off-by: szalpal <mszolucha@nvidia.com>

--- qa/identity/identity_client.py
+++ qa/identity/identity_client.py
@@ -25,6 +25,7 @@ import argparse, os, sys
 import numpy as np
 import tritongrpcclient
 from PIL import Image
+import math
 
 
 def parse_args():
@@ -39,38 +40,7 @@ def parse_args():
                         help='Number of iterations , with `batch_size` size')
     parser.add_argument('--model_name', type=str, required=False, default="dali_backend",
                         help='Model name')
-    img_group = parser.add_mutually_exclusive_group()
-    img_group.add_argument('--img', type=str, required=False, default=None,
-                           help='Run a img dali pipeline. Arg: path to the image.')
-    img_group.add_argument('--img_dir', type=str, required=False, default=None,
-                           help='Directory, with images that will be broken down into batches an infered. The directory must contain images only')
     return parser.parse_args()
-
-
-def load_image(img_path: str):
-    """
-    Loads image as an encoded array of bytes.
-    This is a typical approach you want to use in DALI backend
-    """
-    with open(img_path, "rb") as f:
-        img = f.read()
-        return np.array(list(img)).astype(np.uint8)
-
-
-def load_images(dir_path: str):
-    """
-    Loads all files in given dir_path. Treats them as images
-    """
-    images = []
-
-    # Traverses directory for files (not dirs) and returns full paths to them
-    path_generator = (os.path.join(dir_path, f) for f in os.listdir(dir_path) if
-                      os.path.isfile(os.path.join(dir_path, f)))
-
-    img_paths = [dir_path] if os.path.isfile(dir_path) else list(path_generator)
-    for img in img_paths:
-        images.append(load_image(img))
-    return images
 
 
 def array_from_list(arrays):
@@ -99,14 +69,6 @@ def batcher(dataset, batch_size, n_iterations=-1):
         yield dataset[i * batch_size:(i + 1) * batch_size]
 
 
-def save_byte_image(bytes, size_wh=(224, 224), name_suffix=0):
-    """
-    Utility function, that can be used to save byte array as an image
-    """
-    im = Image.frombytes("RGB", size_wh, bytes, "raw")
-    im.save("result_img_" + str(name_suffix) + ".jpg")
-
-
 def main():
     FLAGS = parse_args()
     try:
@@ -118,24 +80,21 @@ def main():
     model_name = FLAGS.model_name
     model_version = -1
 
-    print("Loading images")
-
-    image_data = load_images(FLAGS.img_dir if FLAGS.img_dir is not None else FLAGS.img)
-    image_data = array_from_list(image_data)
-
-    print("Images loaded, inferring")
+    input_data = [np.random.randint(0, 255, size=np.random.randint(100), dtype='uint8') for _ in
+                  range(np.random.randint(100) * FLAGS.batch_size)]
+    input_data = array_from_list(input_data)
 
     # Infer
     inputs = []
     outputs = []
-    input_name = "INPUT"
-    output_name = "OUTPUT"
-    input_shape = list(image_data.shape)
+    input_name = "DALI_INPUT_0"
+    output_name = "DALI_OUTPUT_0"
+    input_shape = list(input_data.shape)
     input_shape[0] = FLAGS.batch_size
     inputs.append(tritongrpcclient.InferInput(input_name, input_shape, "UINT8"))
     outputs.append(tritongrpcclient.InferRequestedOutput(output_name))
 
-    for batch in batcher(image_data, FLAGS.batch_size):
+    for batch in batcher(input_data, FLAGS.batch_size):
         print("Input mean before backend processing:", np.mean(batch))
         # Initialize the data
         inputs[0].set_data_from_numpy(batch)
@@ -149,18 +108,16 @@ def main():
         output0_data = results.as_numpy(output_name)
         print("Output mean after backend processing:", np.mean(output0_data))
         print("Output shape: ", np.shape(output0_data))
-        maxs = np.argmax(output0_data, axis=1)
-        for i in range(len(maxs)):
-            print("Sample ", i, " - label: ", maxs[i], " ~ ", output0_data[i, maxs[i]])
-            if maxs[i] != 372:
-                sys.exit(1)
+        if not math.isclose(np.mean(output0_data), np.mean(batch)):
+            print("Pre/post average does not match")
+            sys.exit(1)
+        else:
+            print("pass")
 
     statistics = triton_client.get_inference_statistics(model_name=model_name)
     if len(statistics.model_stats) != 1:
         print("FAILED: Inference Statistics")
         sys.exit(1)
-
-    print('PASS: infer')
 
 
 if __name__ == '__main__':
