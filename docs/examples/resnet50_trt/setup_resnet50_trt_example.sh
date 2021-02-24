015c7c83107bd80fa092c09f4e78e3c6ff945877
szalpal <mszolucha@nvidia.com>
2021-02-24 14:34:30 +0100

    fix setup script
    Signed-off-by: szalpal <mszolucha@nvidia.com>

--- docs/examples/resnet50_trt/setup_resnet50_trt_example.sh
+++ docs/examples/resnet50_trt/setup_resnet50_trt_example.sh
@@ -27,8 +27,7 @@ mkdir -p model_repository/resnet50_trt/1
 
 docker run -it --gpus=all -v $(pwd):/workspace nvcr.io/nvidia/pytorch:20.12-py3 /bin/bash -c \
   "python onnx_exporter.py --save model.onnx &&
-   trtexec --onnx=model.onnx --saveEngine=./model_repository/resnet50_trt/1/model.plan
-           --explicitBatch --minShapes=input:1x3x224x224 --optShapes=input:1x3x224x224 --maxShapes=input:256x3x224x224 --fp16 &&
+   trtexec --onnx=model.onnx --saveEngine=./model_repository/resnet50_trt/1/model.plan --explicitBatch --minShapes=input:1x3x224x224 --optShapes=input:1x3x224x224 --maxShapes=input:256x3x224x224 --fp16 &&
    python serialize_dali_pipeline.py --save ./model_repository/dali/1/model.dali"
 
 echo "Resnet50 model ready."
