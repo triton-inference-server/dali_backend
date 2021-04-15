e780af58e155f2647177b20d51d488297097ae05
szalpal <mszolucha@nvidia.com>
2021-04-15 17:53:03 +0200

    working version
    Signed-off-by: szalpal <mszolucha@nvidia.com>

--- client/utils.py
+++ client/utils.py
@@ -21,24 +21,80 @@
 
 import numpy as np
 
-def batcher(dataset, batch_size, n_iterations=-1):
+
+def _select_batch_size(batch_size_provider, batch_idx):
+    if callable(batch_size_provider):
+        return batch_size_provider()
+    elif isinstance(batch_size_provider, list):
+        return batch_size_provider[batch_idx % len(batch_size_provider)]
+    elif isinstance(batch_size_provider, int):
+        return batch_size_provider
+    raise TypeError("Incorrect batch_size_provider type. Actual: ", type(batch_size_provider))
+
+
+def batcher(dataset, batch_size_provider, n_iterations=-1):
     """
     Generator, that splits dataset into batches with given batch size
 
     :param dataset: list of ndarrays, where every ndarray is one sample
-    :param batch_size: Requested batch size
+    :param batch_size_provider: Provides sizes for every batch.
+                                If the argument is a scalar, every batch will have the same size.
+                                If a list, sizes will be picked consecutively
+                                (round-robin if necessary).
+                                If a callable, every iteration will call ``batch_size_provider``
+                                to obtain the size.
     :param n_iterations: Requested number of iterations. Samples gathered in ``dataset`` argument
                          will be used in round-robin manner to form the proper number of batches.
-                         Default value (-1) means, that number of iterations will be calculated
-                         using the formula: ``n_iterations = len(dataset) // batch_size``
+                         Default value (-1) means, that number of iterations will be inferred from
+                         the dataset size - every sample will be used at most one time and last
+                         (incomplete) batch will be dropped.
     :return: Yields batches
     """
-    n_batches = len(dataset) // batch_size if n_iterations == -1 else n_iterations
-    dataset_idx = 0
-    for _ in range(n_batches):
-        if dataset_idx + batch_size < len(dataset):
-            yield dataset[dataset_idx: dataset_idx + batch_size]
-        else:
-            yield np.concatenate((np.repeat(dataset, repeats=batch_size // len(dataset), axis=0),
-                                  dataset[:batch_size % len(dataset)]))
-        dataset_idx += batch_size
+    iter_idx = 0
+    curr_sample = 0
+
+    if n_iterations == -1:
+        while True:
+            batch_size = _select_batch_size(batch_size_provider, iter_idx)
+            if curr_sample + batch_size > len(dataset):
+                return
+            yield dataset[curr_sample: curr_sample + batch_size]
+            curr_sample += batch_size
+            iter_idx += 1
+    else:
+        while True:
+            batch_size = _select_batch_size(batch_size_provider, iter_idx)
+            print(batch_size)
+            if iter_idx >= n_iterations:
+                return
+            elif curr_sample + batch_size < len(dataset):
+                yield dataset[curr_sample: curr_sample + batch_size]
+            else:
+                suffix = len(dataset) - curr_sample
+                n_rep = (batch_size - suffix) // len(dataset)
+                prefix = batch_size - (suffix + len(dataset) * n_rep)
+                yield np.concatenate(
+                    (dataset[curr_sample:],
+                     np.repeat(dataset, repeats=n_rep, axis=0),
+                     dataset[:prefix])
+                )
+            curr_sample = (curr_sample + batch_size) % len(dataset)
+            iter_idx += 1
+
+
+
+def gen():
+    global j
+    j = 2 if j == 1 else 1
+    return j
+
+
+def main():
+    dataset = np.array([i for i in range(5)])
+    for b in batcher(dataset, [3], 4):
+        print(b)
+
+
+if __name__ == '__main__':
+    main()
+
