ceea821fb9afd503107cee5ffd6be210b81695e3
szalpal <mszolucha@nvidia.com>
2021-04-15 18:01:49 +0200

    this batcher is a pro batcher
    Signed-off-by: szalpal <mszolucha@nvidia.com>

--- client/utils.py
+++ client/utils.py
@@ -38,10 +38,10 @@ def batcher(dataset, batch_size_provider, n_iterations=-1):
 
     :param dataset: list of ndarrays, where every ndarray is one sample
     :param batch_size_provider: Provides sizes for every batch.
-                                If the argument is a scalar, every batch will have the same size.
-                                If a list, sizes will be picked consecutively
+                                * If the argument is a scalar, every batch will have the same size.
+                                * If a list, sizes will be picked consecutively
                                 (round-robin if necessary).
-                                If a callable, every iteration will call ``batch_size_provider``
+                                * If a callable, every iteration will call ``batch_size_provider``
                                 to obtain the size.
     :param n_iterations: Requested number of iterations. Samples gathered in ``dataset`` argument
                          will be used in round-robin manner to form the proper number of batches.
@@ -52,49 +52,30 @@ def batcher(dataset, batch_size_provider, n_iterations=-1):
     """
     iter_idx = 0
     curr_sample = 0
+    while True:
+        batch_size = _select_batch_size(batch_size_provider, iter_idx)
 
-    if n_iterations == -1:
-        while True:
-            batch_size = _select_batch_size(batch_size_provider, iter_idx)
-            if curr_sample + batch_size > len(dataset):
+        # Stop condition
+        if n_iterations == -1:
+            if curr_sample + batch_size >= len(dataset):
                 return
-            yield dataset[curr_sample: curr_sample + batch_size]
-            curr_sample += batch_size
-            iter_idx += 1
-    else:
-        while True:
-            batch_size = _select_batch_size(batch_size_provider, iter_idx)
-            print(batch_size)
+        else:
             if iter_idx >= n_iterations:
                 return
-            elif curr_sample + batch_size < len(dataset):
-                yield dataset[curr_sample: curr_sample + batch_size]
-            else:
-                suffix = len(dataset) - curr_sample
-                n_rep = (batch_size - suffix) // len(dataset)
-                prefix = batch_size - (suffix + len(dataset) * n_rep)
-                yield np.concatenate(
-                    (dataset[curr_sample:],
-                     np.repeat(dataset, repeats=n_rep, axis=0),
-                     dataset[:prefix])
-                )
-            curr_sample = (curr_sample + batch_size) % len(dataset)
-            iter_idx += 1
 
-
-
-def gen():
-    global j
-    j = 2 if j == 1 else 1
-    return j
-
-
-def main():
-    dataset = np.array([i for i in range(5)])
-    for b in batcher(dataset, [3], 4):
-        print(b)
-
-
-if __name__ == '__main__':
-    main()
-
+        if curr_sample + batch_size < len(dataset):
+            yield dataset[curr_sample: curr_sample + batch_size]
+        else:
+            # Get as many samples from this revolution of the dataset as possible,
+            # then repeat the dataset as many revolutions as needed
+            # and finally take the remaining samples from the last revolution
+            suffix = len(dataset) - curr_sample
+            n_rep = (batch_size - suffix) // len(dataset)
+            prefix = batch_size - (suffix + len(dataset) * n_rep)
+            yield np.concatenate(
+                (dataset[curr_sample:],
+                 np.repeat(dataset, repeats=n_rep, axis=0),
+                 dataset[:prefix])
+            )
+        curr_sample = (curr_sample + batch_size) % len(dataset)
+        iter_idx += 1
