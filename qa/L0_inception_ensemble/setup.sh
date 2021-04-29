1d6b124e134af22603e8f8df1724916fb524858a
szalpal <mszolucha@nvidia.com>
2021-04-30 00:54:31 +0200

    Fix typo in the setup script
    Signed-off-by: szalpal <mszolucha@nvidia.com>

--- qa/L0_inception_ensemble/setup.sh
+++ qa/L0_inception_ensemble/setup.sh
@@ -8,7 +8,7 @@ mkdir -p ensemble_dali_inception/1
 wget -O /tmp/inception_v3_2016_08_28_frozen.pb.tar.gz \
      https://storage.googleapis.com/download.tensorflow.org/models/inception_v3_2016_08_28_frozen.pb.tar.gz
 (cd /tmp && tar xzf inception_v3_2016_08_28_frozen.pb.tar.gz)
-mv /tmp/inception_v3_2016_08_28_frozen.pb model_repository/inception_graphdef/1/model.graphdef
+mv /tmp/inception_v3_2016_08_28_frozen.pb inception_graphdef/1/model.graphdef
 
 python inception_pipeline.py dali/1/model.dali
 popd
