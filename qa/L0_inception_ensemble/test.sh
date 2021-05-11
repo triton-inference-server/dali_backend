93e30b251e2c140a3ad0496b5e72bccd0743c920
Michał <7101064+szalpal@users.noreply.github.com>
2021-05-11 19:06:18 +0200

    Specify tritonserver address:port in test scripts (#57)
    Signed-off-by: szalpal <mszolucha@nvidia.com>

--- qa/L0_inception_ensemble/test.sh
+++ qa/L0_inception_ensemble/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -ex
 
-echo "Run test"
-python ensemble_client.py --batch_size 64 --n_iter 3 --model_name ensemble_dali_inception --img_dir images
+: ${GRPC_ADDR:=${1:-"localhost:8001"}}
+
+python ensemble_client.py --batch_size 64 --n_iter 3 --model_name ensemble_dali_inception --img_dir images -u $GRPC_ADDR
--- qa/L0_multi_input/test.sh
+++ qa/L0_multi_input/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -ex
 
-echo "Test run"
-python multi_input_client.py --batch_size 256 --n_iter 7
+: ${GRPC_ADDR:=${1:-"localhost:8001"}}
+
+python multi_input_client.py --batch_size 256 --n_iter 7 -u $GRPC_ADDR
