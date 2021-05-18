5bb7cf31bab59644182f45d4f5216ad546e887f8
szalpal <mszolucha@nvidia.com>
2021-05-18 18:58:44 +0200

    wip
    Signed-off-by: szalpal <mszolucha@nvidia.com>

--- qa/L0_resnet50_trt/setup.sh
+++ qa/L0_resnet50_trt/setup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash -ex
 
 pushd model_repository
-source setup_resnet50_trt_example.sh
+
+mkdir -p dali/1
+mkdir -p ensemble_dali_resnet50/1
+mkdir -p resnet50_trt/1
+
+python onnx_exporter.py --save model.onnx
+trtexec --onnx=model.onnx --saveEngine=./resnet50_trt/1/model.plan --explicitBatch --minShapes=input:1x3x224x224 --optShapes=input:1x3x224x224 --maxShapes=input:256x3x224x224 --fp16
+python serialize_dali_pipeline.py --save ./dali/1/model.dali
+
+echo "Resnet50 model ready."
+
 popd
