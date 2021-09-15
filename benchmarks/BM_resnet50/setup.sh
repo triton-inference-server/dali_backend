90beda7c1946836dc966509f485d50bb17ba0b10
Rafal <Banas.Rafal97@gmail.com>
2021-09-15 15:49:15 +0200

    fix typos
    Signed-off-by: Rafal <Banas.Rafal97@gmail.com>

--- benchmarks/BM_resnet50/setup.sh
+++ benchmarks/BM_resnet50/setup.sh
@@ -24,32 +24,32 @@
 set -ex
 
 source ${DALI_BACKEND_REPO_ROOT}/qa/setup_dali_extra.sh
-echo "Succesfully prepared DALI_extra"
+echo "Successfully prepared DALI_extra"
 
 CHECKPOINT_DIR=${CHECKPOINT_DIR:-'checkpoints'}
 
 source scripts/download_checkpoint.sh
-echo "Succesfully downloaded model checkpoints"
+echo "Successfully downloaded model checkpoints"
 
 # Setup tensorflow model
 mkdir -p model_repository/resnet50_tf/1
 cp -r ${CHECKPOINT_DIR}/saved_model/nvidia_rn50_tf_amp model_repository/resnet50_tf/1/model.savedmodel
-echo "Succesfully set up tensorflow model"
+echo "Successfully set up tensorflow model"
 
 # Setup ONNX model
 mkdir -p model_repository/resnet50_onnx/1
 python3 -m tf2onnx.convert --saved-model ${CHECKPOINT_DIR}/saved_model/nvidia_rn50_tf_amp/ --output model_repository/resnet50_onnx/1/model.onnx
-echo "Succesfully set up ONNX model"
+echo "Successfully set up ONNX model"
 
 # Setup TensorRT model
 mkdir -p model_repository/resnet50_trt/1
 trtexec --onnx=model_repository/resnet50_onnx/1/model.onnx --saveEngine=model_repository/resnet50_trt/1/model.plan  --explicitBatch --minShapes=\'input\':1x224x224x3 --optShapes=\'input\':64x224x224x3 --maxShapes=\'input\':128x224x224x3
-echo "Succesfully set up TensorRT model"
+echo "Successfully set up TensorRT model"
 
 # Setup DALI preprocessing model
 mkdir -p model_repository/dali/1
 python3 model_repository/dali/pipeline.py model_repository/dali/1/model.dali
-echo "Succesfully set up DALI preprocessing model"
+echo "Successfully set up DALI preprocessing model"
 
 # Setup DALI + TRT ensemble
 mkdir -p model_repository/dali_trt_resnet50/1
