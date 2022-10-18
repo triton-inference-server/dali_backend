8e8377056629c9f831e4541224e9fa9886b05294
Rafal <rbanas@nvidia.com>
2022-10-18 11:49:29 +0200

    Add a test for autoconfig without configuration file
    Signed-off-by: Rafal <rbanas@nvidia.com>

--- qa/L0_autoconfig/setup.sh
+++ qa/L0_autoconfig/setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 # The MIT License (MIT)
 #
@@ -20,5 +20,9 @@
 # COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+mkdir -p model_repository/no_config_file/1
+python model_repository/no_config_file/pipeline.py model_repository/no_config_file/1/model.dali
+echo "no_config_file model ready"
 
 echo "models ready"
