fd191aac5ff577a2f43490d5812d9783a56713d9
szalpal <mszolucha@nvidia.com>
2023-07-18 15:37:04 +0200

    ResNet 101 benchmark
    Signed-off-by: szalpal <mszolucha@nvidia.com>

---
+++ benchmarks/BM_resnet101/run-benchmarks.sh
@@ -0,0 +1,63 @@
+#!/bin/bash -ex
+
+# The MIT License (MIT)
+#
+# Copyright (c) 2021 NVIDIA CORPORATION & AFFILIATES
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+: ${GRPC_ADDR:=${1:-"localhost:8001"}}
+
+
+
+load_models() {
+  echo "LOAD MODELS"
+  python scripts/model-loader.py -u "${GRPC_ADDR}" load -m dali_preprocessing
+  python scripts/model-loader.py -u "${GRPC_ADDR}" load -m resnet101
+  python scripts/model-loader.py -u "${GRPC_ADDR}" load -m dali_postprocessing
+  python scripts/model-loader.py -u "${GRPC_ADDR}" load -m segmentation_bls
+}
+
+unload_models() {
+  echo "UNLOAD MODELS"
+  python scripts/model-loader.py -u "${GRPC_ADDR}" unload -m segmentation_bls
+  python scripts/model-loader.py -u "${GRPC_ADDR}" unload -m dali_postprocessing
+  python scripts/model-loader.py -u "${GRPC_ADDR}" unload -m resnet101
+  python scripts/model-loader.py -u "${GRPC_ADDR}" unload -m dali_preprocessing
+}
+
+TIME_WINDOW=10000
+BATCH_SIZES="1 2 4 6 8 16"
+#BATCH_SIZES="2 8 16 32 64 128"
+
+PERF_ANALYZER_ARGS="-i grpc -u $GRPC_ADDR -p$TIME_WINDOW"
+
+#echo "WARMUP"
+#perf_analyzer $PERF_ANALYZER_ARGS -m segmentation_bls --input-data test_sample --shape encoded:`stat --printf="%s" test_sample/encoded` -b 1
+
+#echo "ResNet101 Benchmark: single-sample"
+#perf_analyzer $PERF_ANALYZER_ARGS -m segmentation_bls --input-data test_sample --shape encoded:`stat --printf="%s" test_sample/encoded` --concurrency-range=16:128:16
+
+echo "ResNet101 Benchmark: batched"
+for BS in $BATCH_SIZES ; do
+  load_models
+  sleep 5
+  perf_analyzer $PERF_ANALYZER_ARGS -m segmentation_bls --input-data test_sample --shape encoded:`stat --printf="%s" test_sample/encoded` -b$BS ;
+  unload_models
+  sleep 5
+done
